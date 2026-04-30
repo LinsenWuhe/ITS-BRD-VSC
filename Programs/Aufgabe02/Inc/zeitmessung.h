a9ed4daedad710f7c89144a5df9407d1dd4e8561
a-a-haw <Ahmet.Altiparmak@haw-hamburg.de>
2026-04-30 10:11:17 +0200

    zeitmessung erweitert

--- Programs/Aufgabe02/Inc/zeitmessung.h
+++ Programs/Aufgabe02/Inc/zeitmessung.h
@@ -1,4 +1,15 @@
 #ifndef __ZEITMESSUNG_H
 #define __ZEITMESSUNG_H
 
+#include "timer.h"
+#include <stdint.h>
+
+int anfangsZeit; // oder uint32_t?
+
+int initZeitmessung();
+
+int setzteZeitpunk();
+
+int differenzZumZeitpunkt(int* differenz);
+
 #endif
--- Programs/Aufgabe02/Src/zeitmessung.c
+++ Programs/Aufgabe02/Src/zeitmessung.c
@@ -1,1 +1,22 @@
 #include "zeitmessung.h"
+#include "timer.h"
+
+int initZeitmessung()
+{
+    initTimer();
+
+    return 0;
+}
+
+int setzteZeitpunk()
+{
+    anfangsZeit = getTimeStamp();
+    return 0;
+}
+
+// TODO: in ms umwandeln
+int differenzZumZeitpunktInms(int* differenz)
+{
+    *differenz = getTimeStamp() - anfangsZeit;
+    return 0;
+}
