69c6e6d8579e23dfd21a1cb93d7c10164689127a
Jannik <meine@email.de>
2026-05-31 16:37:13 +0200

    bmp reader.h für lesen von header

---
+++ Programs/Aufgabe3/Inc/bmp_reader.h
@@ -0,0 +1,29 @@
+#ifndef _BMP_READER_H
+#define _BMP_READER_H
+
+#include "BMP_types.h"
+
+/**
+*Liest und prüft BITMAPFILEHEADER und BITMAPINFOHEADER
+* fh zeigt auf Bitmapfileheader
+* ih zeigt auf Bitmapinfoheader
+*/
+int BMP_readHeaders(BITMAPFILEHEADER *fh, BITMAPFILEHEADER *ih);
+
+/**
+* Liest die Farbpalette (8-Bit)
+* ih zeigt auf Bitmapinfoheader
+* palette ist Array für Palette (max. 265 Einträge)
+*/
+int BMP_readPalette(BITMAPINFOHEADER *ih, RGBQUAD *palette);
+
+
+/**
+*Dekodiert das gesamte Bild und gibt Pixel auf Display aus
+* fh ist zeiger auf Bitmapfileheader
+* ih ist zeiger auf Bitmapinfoheader
+* palette ist Farbpalette
+*/
+int BMP_decodeAndDisplay(BITMAPFILEHEADER *fh, BITMAPINFOHEADER *ih, RGBQUAD *palette);
+
+#endif
