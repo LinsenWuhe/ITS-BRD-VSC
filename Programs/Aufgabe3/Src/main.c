c7c1ff6a66250c7497005507685c5d9199abb875
a-a-haw <Ahmet.Altiparmak@haw-hamburg.de>
2026-05-31 12:49:54 +0200

    erste befehle

--- Programs/Aufgabe3/Src/main.c
+++ Programs/Aufgabe3/Src/main.c
@@ -1,20 +1,26 @@
 /**
   ******************************************************************************
   * @file    main.c
-  * @author  Franz Korf
+  * @author  Franz Korf, modifiziert durch andere
   * @brief   Kleines Testprogramm fuer neu erstelle Fonts.
   ******************************************************************************
   */
 /* Includes ------------------------------------------------------------------*/
 
+#include "input.h"
 #include "stm32f4xx_hal.h"
 #include "init.h"
 #include "LCD_GUI.h"
 #include "LCD_Touch.h"
+#include "BMP_types.h"
 #include "lcd.h"
 #include "fontsFLASH.h"
 #include "additionalFonts.h"
 #include "error.h"
+
+#define IDR_MASK_PIN_6 (0x01U << 6)
+#define PIXELSIZE 16
+#define PIXELCOUNT 100
 
 
 int main(void) {
@@ -23,12 +29,23 @@ int main(void) {
 	GUI_init(DEFAULT_BRIGHTNESS);   // Initialisierung des LCD Boards mit Touch
 	TP_Init(false);                 // Initialisierung des LCD Boards mit Touch
 
-  // Begruessungstext	
-	lcdPrintlnS("Hallo liebes TI-Labor (c-project)");
 	
-	// Test in Endlosschleife
+	initInput();
+	GUI_clear(255);
+	
+	char gpiofPin6Pressed;
+
+	char buffer[PIXELCOUNT];
+	
 	while(1) {
-		HAL_Delay(10000);
+		gpiofPin6Pressed = (IDR_MASK_PIN_6 != (GPIOF->IDR & IDR_MASK_PIN_6));
+		
+		if (gpiofPin6Pressed)
+		{
+			openNextFile();
+			COMread(buffer, PIXELSIZE, PIXELCOUNT);
+		}
+
 	}
 }
 
